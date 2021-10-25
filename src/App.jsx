ecd412b5a7e06a90e7d4849f39fc66dcadc65478
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-25 10:56:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,44 +7,33 @@ import Container from './components/Container';
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
 
-class App extends Component {
-  state = {
-    inputValue: '',
-    showModal: false,
-  };
+function App() {
+  const [inputValue, setInputValue] = useState('');
 
-  handleSearchbarSubmit = inputValue => {
-    this.setState({ inputValue, page: 1 });
-  };
+  // const handleSearchbarSubmit = inputValue => {
+  //   setInputValue(inputValue);
+  // };
 
+  return (
+    <>
+      <Searchbar onSubmit={setInputValue} />
 
-  loadMore() {}
+      <ToastContainer
+        theme="colored"
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        progress={undefined}
+      />
 
-  render() {
-    
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSearchbarSubmit} />
-
-        <ToastContainer
-          theme="colored"
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          closeOnClick={true}
-          pauseOnHover={true}
-          draggable={true}
-          progress={undefined}
-        />
-
-        <Container>
-          <ImageGallery inputValue={this.state.inputValue} />
-        </Container>
-
-     
-      </>
-    );
-  }
+      <Container>
+        <ImageGallery inputValue={inputValue} />
+      </Container>
+    </>
+  );
 }
 
 export default App;
